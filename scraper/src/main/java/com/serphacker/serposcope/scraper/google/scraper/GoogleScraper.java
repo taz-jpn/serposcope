b634780da750d68e93dd9946d96d5dfcfe9b3238
kage <kagesan88@gmail.com>
2018-11-04 13:04:57 +0900

    Googleにアクセスするたびに必ずウェイトを入れる

--- scraper/src/main/java/com/serphacker/serposcope/scraper/google/scraper/GoogleScraper.java
+++ scraper/src/main/java/com/serphacker/serposcope/scraper/google/scraper/GoogleScraper.java
@@ -133,9 +133,9 @@ public class GoogleScraper {
                 resultsNumber = parseResultsNumberOnFirstPage();
             }
             
-            if(!hasNextPage()){
-                break;
-            }
+//            if(!hasNextPage()){
+//                break;
+//            }
             
             long pause = search.getRandomPagePauseMS();
             if(pause > 0){
